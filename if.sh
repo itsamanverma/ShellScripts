45289a949cc542c35324aead59e7a8734571b53f
itsamanverma <amanvermame786@gmail.com>
2020-06-04 14:03:46 +0530

    while.sh

--- if.sh
+++ if.sh
@@ -6,6 +6,12 @@ if [[ $COLOR = "blue" ]];
 then
   #statements
   echo "the color is blue"
+elif [[ $COLOR = "red" ]];
+then
+  #statements
+  echo "the color is red not blue"
+else
+  echo "the color is $COLOR not matchs"
 fi
 
 User_Guess=$2
@@ -14,4 +20,10 @@ if [[ $User_Guess -lt $Computer ]];
 then
   #statements
   echo "you are too low"
+elif [[ $User_Guess -gt $Computer ]];
+then
+  #statements
+  echo "you're too high too Compare"
+else
+  echo "you guessed it"
 fi
