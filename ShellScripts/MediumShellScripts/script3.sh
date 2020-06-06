c7ed57d5714de82ff7905ec6a3efd394bedbfcfa
itsamanverma <amanvermame786@gmail.com>
2020-06-06 15:14:59 +0530

    Logical Oparetor with scripts

---
+++ ShellScripts/MediumShellScripts/script3.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#reading data from the user
+read -p 'Enter a : ' a
+read -p 'Enter b : ' b
+
+if(($a=="true" & $b=="true" ))
+then
+	echo Both are true.
+else
+	echo Both are not true.
+fi
+
+if(($a=="true" || $b=="true" ))
+then
+	echo Atleast one of them is true.
+else
+	echo None of them is true.
+fi
+
+if(( ! $a=="true" ))
+then
+	echo "a" was intially false.
+else
+	echo "a" was intially true.
+fi
+
+echo "Logical Oparetor is terminate "
+exit 0
