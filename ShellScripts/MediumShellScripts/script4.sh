a2a6158a808d85b048ba27e6d98e410b7764b4f4
itsamanverma <amanvermame786@gmail.com>
2020-06-06 15:29:32 +0530

    Bitwise Operators with scripts

---
+++ ShellScripts/MediumShellScripts/script4.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+#reading data from the user
+read -p 'Enter a : ' a
+read -p 'Enter b : ' b
+
+bitwiseAND=$(( a&b ))
+echo Bitwise AND of a and b is $bitwiseAND
+
+bitwiseOR=$(( a|b ))
+echo Bitwise OR of a and b is $bitwiseOR
+
+bitwiseXOR=$(( a^b ))
+echo Bitwise XOR of a and b is $bitwiseXOR
+
+bitiwiseComplement=$(( ~a ))
+echo Bitwise Compliment of a is $bitiwiseComplement
+
+leftshift=$(( a<<1 ))
+echo Left Shift of a is $leftshift
+
+rightshift=$(( b>>1 ))
+echo Right Shift of b is $rightshift
+
+echo "Bitwise Operators is terminate"
+exit 0
