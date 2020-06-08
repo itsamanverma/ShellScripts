f020b6b1fe29b615519464ab5265ceb4199d8c3a
itsamanverma <amanvermame786@gmail.com>
2020-06-08 19:41:22 +0530

    bubble sort

---
+++ ShellScripts/AdvanceShellScripts/script12.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+# Program in Bash to 
+# print pyramid 
+
+# Static input to the 
+# number 
+read -p "Enter the Rows:" p 
+
+for((m=1; m<=p; m++)) 
+do
+	# This loop print spaces 
+	# required 
+	for((a=i; a<=p; a++)) 
+	do
+	echo -ne " "; 
+	done
+	
+	# This loop print the left 
+	# side of the pyramid 
+	for((n=1; n<=m; n++)) 
+	do
+	echo -ne "#"; 
+	done
+
+	# This loop print right 
+	# side of the pryamid. 
+	for((i=1; i<m; i++)) 
+	do
+	echo -ne "#"; 
+	done
+
+	# New line 
+	echo; 
+done
+
