d5e30487762dac28c3f805edbc985ad433e34ce6
itsamanverma <amanvermame786@gmail.com>
2020-06-08 16:41:26 +0530

    reverse the strings using the scripts

---
+++ ShellScripts/AdvanceShellScripts/script3.sh
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env bash 
+#reading a string 
+read -p "Enter string:" string 
+	#getting the length of given string 
+	len=${#string} 
+#looping for reversing a string 
+#initialize i=len-1 for reversing a string and run till i=0 
+#printing in the reverse order of the given string 
+for ((i = $len - 1; i >= 0; i--)) 
+	do
+	 #"${string:$i:1}"extract single single character from string. 
+	 reverse="$reverse${string:$i:1}" 
+	done 
+		echo "$reverse"
